9dabdb4bbce16e99fabf5dda682cdadf2edbea14
Lucas Reginato Dev <lucasreginatodev@gmail.com>
2026-01-09 17:02:20 -0300

    feat: add sign in with google

--- app/_components/ui/sidebar-sheet.tsx
+++ app/_components/ui/sidebar-sheet.tsx
@@ -1,21 +1,9 @@
+"use client";
+
 import { Button } from "./button";
-import {
-  CalendarIcon,
-  HomeIcon,
-  LogInIcon,
-  LogOutIcon,
-  MenuIcon,
-} from "lucide-react";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "./sheet";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
+import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./sheet";
 import { quickSearchOptions } from "@/app/_constants/search";
-import { Avatar, AvatarImage } from "./avatar";
 import Link from "next/link";
 import Image from "next/image";
 import {
@@ -26,8 +14,14 @@ import {
   DialogTrigger,
   DialogHeader,
 } from "@/app/_components/ui/dialog";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarImage } from "./avatar";
 
 const SidebarSheet = () => {
+  const { data } = useSession();
+  const handleLoginWithGoogleClick = () => signIn("google");
+  const handleLogoutClick = () => signOut();
+
   return (
     <SheetContent className="overflow-y-auto">
       <SheetHeader>
@@ -35,41 +29,55 @@ const SidebarSheet = () => {
       </SheetHeader>
 
       <div className="flex justify-between items-center p-4 pt-0 border-b border-solid">
-        <h2 className="font-bold text-lg">Olá, Faça seu Login!</h2>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button size="icon">
-              <LogInIcon size={18} />
-            </Button>
-          </DialogTrigger>
-
-          <DialogContent className="w-[80%]">
-            <DialogHeader>
-              <DialogTitle>Faça login na plataforma.</DialogTitle>
-              <DialogDescription>
-                Conecte-se usando sua conta Google.
-              </DialogDescription>
-            </DialogHeader>
-
-            <Button variant="outline" className="gap-1 font-bold">
-              <Image
-                src="/google.svg"
-                alt="Fazer Login com Google"
-                width={18}
-                height={18}
+        {data?.user ? (
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage
+                src={data?.user?.image ?? ""}
+                alt="Avatar do usuário"
               />
-              Continuar com Google
-            </Button>
-          </DialogContent>
-        </Dialog>
-        {/* <Avatar>
-          <AvatarImage src="/avatar-01.png" alt="Avatar do usuário" />
-        </Avatar>
+            </Avatar>
 
-        <div>
-          <p className="font-bold px-3">Lucas Blex</p>
-          <p className="text-xs px-3">lucasblex@gmail.com</p>
-        </div> */}
+            <div>
+              <p className="font-bold px-3">{data?.user?.name}</p>
+              <p className="text-xs px-3">{data?.user?.email}</p>
+            </div>
+          </div>
+        ) : (
+          <>
+            <h2 className="font-bold text-lg">Olá, Faça seu Login!</h2>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size="icon">
+                  <LogInIcon size={18} />
+                </Button>
+              </DialogTrigger>
+
+              <DialogContent className="w-[80%]">
+                <DialogHeader>
+                  <DialogTitle>Faça login na plataforma.</DialogTitle>
+                  <DialogDescription>
+                    Conecte-se usando sua conta Google.
+                  </DialogDescription>
+                </DialogHeader>
+
+                <Button
+                  variant="outline"
+                  className="gap-1 font-bold"
+                  onClick={handleLoginWithGoogleClick}
+                >
+                  <Image
+                    src="/google.svg"
+                    alt="Fazer Login com Google"
+                    width={18}
+                    height={18}
+                  />
+                  Continuar com Google
+                </Button>
+              </DialogContent>
+            </Dialog>
+          </>
+        )}
       </div>
 
       <div className="flex flex-col p-5 gap-4 border-b border-solid">
@@ -83,7 +91,7 @@ const SidebarSheet = () => {
         </SheetClose>
         <Button className="gap-1 justify-start" variant="ghost">
           <CalendarIcon size={18} />
-          Agendamento
+          Agendamentos
         </Button>
       </div>
 
@@ -106,7 +114,11 @@ const SidebarSheet = () => {
       </div>
 
       <div className="flex flex-col gap-2 p-5">
-        <Button className="gap-2 justify-start" variant="ghost">
+        <Button
+          className="gap-2 justify-start"
+          variant="ghost"
+          onClick={handleLogoutClick}
+        >
           <LogOutIcon size={18} />
           Sair da conta
         </Button>
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,17 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "app/_lib/prisma";
-import { Adapter } from "next-auth/adapters";
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(db) as Adapter,
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
 });
-
-export { handler as GET, handler as POST };
