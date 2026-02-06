28f0e1a2f7320da9f41e2a57a5ca656c161e8540
Lucas Reginato Dev <lucasreginatodev@gmail.com>
2026-02-06 20:39:34 -0300

    feat: ensure booking can be created

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -14,4 +14,10 @@ export const {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
 });
